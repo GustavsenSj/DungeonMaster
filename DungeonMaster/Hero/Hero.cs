010404af693eb8ba963bccb8d92b0200b15db24c
Sjur G <sjur.gustavsen@no.experis.com>
2023-09-13 13:46:25 +0200

    Refactor HeroAttributes

--- DungeonMaster/Hero/Hero.cs
+++ DungeonMaster/Hero/Hero.cs
@@ -3,37 +3,7 @@ using DungeonMaster.Exceptions;
 
 namespace DungeonMaster.Hero;
 
-/// <summary>
-/// Contains the different attributes a hero/player can have  
-/// </summary>
-public class HeroAttributes
-{
-    public int Strength;
-    public int Dexterity;
-    public int Intelligence;
 
-    public HeroAttributes(int strength, int dexterity, int intelligence)
-    {
-        Strength = strength;
-        Dexterity = dexterity;
-        Intelligence = intelligence;
-    }
-
-    public void AddToStrength(int toAdd)
-    {
-        Strength += toAdd;
-    }
-
-    public void AddToDex(int toAdd)
-    {
-        Dexterity += toAdd;
-    }
-
-    public void AddToInt(int toAdd)
-    {
-        Intelligence += toAdd;
-    }
-}
 
 /// <summary>
 /// Hero interface represents the core values needed to create a hero/player
---
+++ DungeonMaster/Hero/HeroAttributes.cs
@@ -0,0 +1,33 @@
+﻿namespace DungeonMaster.Hero;
+
+/// <summary>
+/// Contains the different attributes a hero/player can have  
+/// </summary>
+public class HeroAttributes
+{
+    public int Strength;
+    public int Dexterity;
+    public int Intelligence;
+
+    public HeroAttributes(int strength, int dexterity, int intelligence)
+    {
+        Strength = strength;
+        Dexterity = dexterity;
+        Intelligence = intelligence;
+    }
+
+    public void AddToStrength(int toAdd)
+    {
+        Strength += toAdd;
+    }
+
+    public void AddToDex(int toAdd)
+    {
+        Dexterity += toAdd;
+    }
+
+    public void AddToInt(int toAdd)
+    {
+        Intelligence += toAdd;
+    }
+}
